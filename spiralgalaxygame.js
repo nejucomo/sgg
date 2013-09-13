4f73812272fb6460d8f30f718d7d09d2c350660a
Nathan Wilcox <nejucomo@gmail.com>
2013-09-12 18:26:35 -0700

    Start pulling out galaxy generation parameters.

--- spiralgalaxygame.js
+++ spiralgalaxygame.js
@@ -11,17 +11,24 @@ window.addEventListener(
       };
     starradius.range = starradius.max - starradius.min;
 
+    var param = {
+      tightness: 0.35,
+      spokes: 3,
+      spin: 2,
+      diffusion: 1.5,
+    };
+
     var radius_distribution = function (u) {
-      var a = Math.pow(Math.sin(0.5 * Math.PI * u), 0.35);
+      var a = Math.pow(Math.sin(0.5 * Math.PI * u), param.tightness);
       var b = u * u;
 
       return (a + b*b) / 2;
     };
 
     var angle_distribution = function (u) {
-      var factor = 3 * Math.PI;
+      var factor = param.spokes * Math.PI;
       var a = Math.sin(factor * u);
-      var b = Math.pow(Math.abs(a), 1.5);
+      var b = Math.pow(Math.abs(a), param.diffusion);
       return u - b / factor;
     };
 
@@ -54,7 +61,7 @@ window.addEventListener(
       var radius = radius_distribution(Math.random())
       var angle = 2 * Math.PI * angle_distribution(Math.random());
       var srad = star_radius_distribution(Math.random());
-      var spinangle = angle + 2*radius;
+      var spinangle = angle + param.spin * radius;
 
       var x = halfsize + halfsize * radius * Math.cos(spinangle);
       var y = halfsize + halfsize * radius * Math.sin(spinangle);
