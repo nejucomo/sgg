f911ecb2fb06b4325f4b435a887f81710cbc94bf
Nathan Wilcox <nejucomo@gmail.com>
2013-09-12 18:21:09 -0700

    Make stars variable radius; Turn of distribution plots.

--- spiralgalaxygame.js
+++ spiralgalaxygame.js
@@ -5,6 +5,11 @@ window.addEventListener(
     var mapview = document.getElementById('mapview');
     var viewsize = 1000;
     var halfsize = viewsize / 2;
+    var starradius = {
+      min: viewsize / 4000,
+      max: viewsize / 800,
+      };
+    starradius.range = starradius.max - starradius.min;
 
     var radius_distribution = function (u) {
       var a = Math.pow(Math.sin(0.5 * Math.PI * u), 0.35);
@@ -20,30 +25,40 @@ window.addEventListener(
       return u - b / factor;
     };
 
-    var plot = function (x, y, color) {
+    var star_radius_distribution = function (u) {
+      return starradius.min + starradius.range * Math.pow(u, 2);
+    };
+
+    var plot = function (x, y, radius, color) {
       var circle = document.createElementNS(svgns, 'circle');
       circle.setAttribute('cx', x.toString());
       circle.setAttribute('cy', y.toString());
-      circle.setAttribute('r', viewsize / 400);
+      circle.setAttribute('r', radius);
       circle.setAttribute('fill', color);
       mapview.appendChild(circle);
     };
 
     // distribution plots:
+    /*
     for (var u = 0; u < 1.0; u += 0.001) {
-      plot(u * viewsize, viewsize - angle_distribution(u) * viewsize, 'blue');
-      plot(u * viewsize, viewsize - radius_distribution(u) * viewsize, 'green');
+      var x = u * viewsize;
+      var y_of = function (yu) { return viewsize - yu * viewsize };
+
+      plot(x, y_of(angle_distribution(u)), starradius.max, 'blue');
+      plot(x, y_of(radius_distribution(u)), starradius.max, 'green');
     };
+    */
 
     // The star distribution:
-    for (var n = 0; n < 1000; n++) {
+    for (var n = 0; n < 3000; n++) {
       var radius = radius_distribution(Math.random())
       var angle = 2 * Math.PI * angle_distribution(Math.random());
+      var srad = star_radius_distribution(Math.random());
       var spinangle = angle + 2*radius;
 
       var x = halfsize + halfsize * radius * Math.cos(spinangle);
       var y = halfsize + halfsize * radius * Math.sin(spinangle);
 
-      plot(x, y, 'white');
+      plot(x, y, srad, 'white');
     }
   });
