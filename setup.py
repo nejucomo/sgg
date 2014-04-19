b2c8bcc3921cca0bd40b89d2ee4121213ca7b346
Nathan Wilcox <nejucomo@gmail.com>
2014-04-19 11:09:49 -0700

    Start transitioning to a twisted web application.

---
+++ setup.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+setup(name='spiralgalaxygame',
+      description='Spiral Galaxy Game',
+      url='https://github.com/nejucomo/spiralgalaxygame',
+      license='GPLv3',
+      version='0.1.dev0',
+      author='Nathan Wilcox',
+      author_email='nejucomo@gmail.com',
+      packages=find_packages(),
+      install_requires=[
+        'twisted >= 13.1',
+        ],
+      entry_points = {
+        'console_scripts': [
+            'sgg = sgg.main:main',
+            ],
+        },
+      package_data = {'sgg': ['web/static/*']},
+      )
---
+++ sgg/main.py
@@ -0,0 +1,60 @@
+#! /usr/bin/env python
+
+import sys
+import argparse
+import logging
+
+import pkg_resources
+
+from twisted.web import server
+from twisted.web.static import File
+from twisted.internet import endpoints
+from twisted import internet
+from twisted import python
+
+
+DESCRIPTION = """
+sgg - The Spiral Galaxy Game!
+"""
+
+
+def main(args = sys.argv[1:]):
+    parse_args(args)
+
+    staticdir = pkg_resources.resource_filename('sgg', 'web/static')
+    site = server.Site(File(staticdir))
+
+    ep = endpoints.serverFromString(internet.reactor, 'tcp:8080')
+    ep.listen(site)
+
+    internet.reactor.run()
+
+
+def parse_args(args):
+    p = argparse.ArgumentParser(
+        description=DESCRIPTION,
+        formatter_class=argparse.RawTextHelpFormatter)
+
+    p.add_argument('--log-level',
+                   dest='loglevel',
+                   default='INFO',
+                   choices=['DEBUG', 'INFO', 'WARN', 'ERROR', 'CRITICAL'],
+                   help='Set logging level.')
+
+    opts = p.parse_args(args)
+
+    logging.basicConfig(
+        stream=sys.stdout,
+        format='%(asctime)s %(levelname) 5s %(name)s | %(message)s',
+        datefmt='%Y-%m-%dT%H:%M:%S%z',
+        level=getattr(logging, opts.loglevel))
+
+    python.log.PythonLoggingObserver().start()
+
+    logging.getLogger('parse_args').debug('Options parsed: %r', opts)
+
+    return opts
+
+
+if __name__ == '__main__':
+    main()
