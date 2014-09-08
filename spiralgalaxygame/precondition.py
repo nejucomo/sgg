12b800b4b958864509f3caa2888b60066117ca29
Nathan Wilcox <nejucomo@gmail.com>
2014-09-08 13:29:22 -0700

    Remove an untested branch, minimizing to fit the test spec.

--- spiralgalaxygame/precondition.py
+++ spiralgalaxygame/precondition.py
@@ -1,11 +1,11 @@
-from types import FunctionType, MethodType
+from types import MethodType
 
 
 class PreconditionError (TypeError):
     def __init__(self, callee, *args):
         if isinstance(callee, MethodType):
             name = '{0.im_class.__name__}.{0.im_func.__name__}'.format(callee)
-        elif isinstance(callee, type) or isinstance(callee, FunctionType):
+        else:
             name = callee.__name__
 
         TypeError.__init__(self, '{}{!r}'.format(name, args))
